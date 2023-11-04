79e5abdabca50be06ddd80167945338d81f9900e
Ryan Steiger <ryan@dontlie.local.com>
2023-11-04 21:52:46 +0100

    -adding my code-along from section 9

---
+++ section-09-working-with-javascript-and-css/shared.js
@@ -0,0 +1,41 @@
+let backdrop = document.querySelector('.backdrop')
+const selectPlanButtons = document.querySelectorAll('.plan button')
+const modalNoButton = document.querySelector('.modal__action--negative')
+const modal = document.querySelector('.modal')
+const toggleButton = document.querySelector('.toggle-button')
+const mobileNav = document.querySelector('.mobile-nav')
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+	selectPlanButtons[i].addEventListener('click', function(){
+		// modal.style.display = 'block'	
+		// backdrop.style.display = 'block'	
+		modal.classList.add('open')
+		backdrop.classList.add('open')
+	})
+}
+
+backdrop.addEventListener('click', function(){
+	// mobileNav.style.display = 'none'
+	mobileNav.classList.remove('open')
+	closeModal()
+})
+
+if (modalNoButton) {
+	modalNoButton.addEventListener('click', closeModal)
+}
+
+function closeModal() {
+	// backdrop.style.display = 'none'
+	// modal.style.display = 'none'
+	if (modal) {
+		modal.classList.remove('open')
+		backdrop.classList.remove('open')
+	}
+}
+
+toggleButton.addEventListener('click', function(){
+	// mobileNav.style.display = 'block'
+	// backdrop.style.display = 'block'
+	mobileNav.classList.add('open')
+	backdrop.classList.add('open')
+})
